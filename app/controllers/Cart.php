077e4929a3bc5e092c4ff21edc4f1d864a42841d
ndranathunga <ranathungand.20@uom.lk>
2023-01-04 18:29:18 +0530

    Cart class

---
+++ app/controllers/Cart.php
@@ -0,0 +1,16 @@
+<?php
+
+class Cart extends Controller {
+    // S_PATH/e_business/cart
+    // constructor
+    // cart/items
+    // cart/checkout
+    // cart/confirm
+    // cart/payment
+    // cart/successful  :placed
+    public function __construct($controller, $action)
+    {
+        parent::__construct($controller, $action);
+    }
+
+}
