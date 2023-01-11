5b3df1821d9b2f42f5b4b918b742f7f735d87ef9
Wathmiv <wathmi.v@gmail.com>
2023-01-11 14:02:21 +0530

    browsing front end

--- app/views/layouts/default1.php
+++ app/views/layouts/default1.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <link rel='stylesheet' href="<?= PROOT ?>css/custom1.css?v=<?php echo time(); ?>" media="screen" title="no title" charset="utf-8">
+    <link rel='stylesheet' href="<?= PROOT ?>css/custom1.css?v=<?php echo time(); ?>" media="screen" title="no title" charset="utf-8">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -19,50 +20,55 @@
     <style>
         /* Formatting search box */
 
-        .result{
+        .result {
             position: absolute;
             z-index: 999;
             top: 100%;
             left: 0;
         }
-        .result{
+
+        .result {
             width: 100%;
             box-sizing: border-box;
         }
+
         /* Formatting result items */
-        .result p{
+        .result p {
             margin: 0;
             padding: 7px 10px;
             border: 1px solid #CCCCCC;
             border-top: none;
             cursor: pointer;
         }
-        .result p:hover{
+
+        .result p:hover {
             background: #f2f2f2;
         }
     </style>
 
     <script>
-        $(document).ready(function(){
-            $('.search-box input[type="text"]').on("keyup input", function(){
+        $(document).ready(function() {
+            $('.search-box input[type="text"]').on("keyup input", function() {
                 var inputVal = $(this).val();
                 var resultDropdown = $(this).siblings(".result");
-                if(inputVal.length){
-                    $.get("<?= PROOT ?>/home/search", {term: inputVal}).done(function(data){
+                if (inputVal.length) {
+                    $.get("<?= PROOT ?>/home/search", {
+                        term: inputVal
+                    }).done(function(data) {
                         resultDropdown.html(data);
                     });
-                } else{
+                } else {
                     resultDropdown.empty();
                 }
             });
 
-            $(document).on("click", ".result p", function(){
+            $(document).on("click", ".result p", function() {
                 $(this).parents(".search-box").find('input[type="text"]').val($(this).text());
                 $(this).parents(".search-box").find('input[type="text"]').attr('url', $(this).attr('id'));
                 $(this).parent(".result").empty();
             });
 
-            $('#search-btn').on('click', function () {
+            $('#search-btn').on('click', function() {
                 alert($('#search-field').attr('url'));
                 window.location.href = $('#search-field').attr('url');
             })
@@ -72,11 +78,11 @@
 
 <body>
     <?php
-        if (isset($_SESSION['isLoggedIn']) && $_SESSION['isLoggedIn']) {
-            require_once "app/views/layouts/nav-loggedIn.php";
-        } else {
-            require_once "app/views/layouts/nav-loggedOut.php";
-        }
+    if (isset($_SESSION['isLoggedIn']) && $_SESSION['isLoggedIn']) {
+        require_once "app/views/layouts/nav-loggedIn.php";
+    } else {
+        require_once "app/views/layouts/nav-loggedOut.php";
+    }
     ?>
     <?= $this->content('body'); ?>
 </body>
--- app/views/layouts/default2.php
+++ app/views/layouts/default2.php
@@ -10,7 +10,7 @@
     <?= $this->content('head'); ?>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel='stylesheet' href="<?= PROOT ?>css/custom.css?v=<?php echo time(); ?>" media="screen" title="no title" charset="utf-8">
+    <link rel='stylesheet' href="<?= PROOT ?>css/custom1.css?v=<?php echo time(); ?>" media="screen" title="no title" charset="utf-8">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
