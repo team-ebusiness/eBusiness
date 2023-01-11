1e24a707ea79a6d9af7af6f2b6a94df6cfee10a1
hansinikarunarathne <107214435+hansinikarunarathne@users.noreply.github.com>
2023-01-11 14:55:31 +0530

    Update config.php

--- config/config.php
+++ config/config.php
@@ -4,16 +4,15 @@ const DEFAULT_CONTROLLER = 'Home'; // default controller if there isn't one defi
 const DEBUG = true; // set to false when in production
 const DEFAULT_LAYOUT = 'default'; // if no layout is set in the controller use this layout
 const SITE_TITLE = 'MVC Framework'; // this will be used if no site title is set
-const PROOT = '/e_business/'; // set the server root
+const PROOT = '/eBusiness/'; // set the server root
 
 
 // database details
-const DB_HOST = 'localhost:3306';
-const DB_NAME = 'e_commerce_db';
+const DB_HOST = '127.0.0.1';
+const DB_NAME = 'ecom';
 const DB_USER = 'root';
-const DB_PASSWORD = 'aiypwzqp';
+const DB_PASSWORD = '';
 
 const CURRENT_USER_SESSION_NAME = 'fhHUHFnjnHHIEnjwwBjhdNnov'; // session name for logged in user
 const REMEMBER_ME_COOKIE_NAME = 'e-commerce-user1'; // cookie name for logged in user remember me
 const REMEMBER_ME_COOKIE_EXPIRY = 604800; // time in seconds for remember me cookie to live (30 days)
-
